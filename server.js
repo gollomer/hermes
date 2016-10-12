75fc2fdf3fcc45814c9e389548b004f14c0bfc6d
gollomer <gollomer@gmail.com>
2016-10-12 08:28:00 -0700

    Seperating namespace for Prometheus

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var url = require("url");
 
 app.get('/', function(req, res){
   res.sendFile('/var/www/projects/projects4me/htdocs/hermes/view.html');
 });
 
-//var nsp = io.of('/clienta');
+app.get('/admin', function(req, res){
+  res.sendFile('/var/www/projects/projects4me/htdocs/hermes/admin.html');
+});
+/*
+io.use(function(socket, next){
+  //console.log(socket.request.headers.origin);
+
+  if (socket.request.headers.origin) {
+    var matches = url.parse(socket.request.headers.origin).host.match(/(([^.]+)\.)?prometheus\.com/);
+    //var matches = origin.host.match(/(([^.]+)\.)?prometheus\.com/);
+
+    var subdomain = null;
+    if (matches !== null){
+      subdomain = matches[2];
+
+      // switch the namespace of the socket
+      console.log(subdomain);
+      socket.nsp.name='/'+subdomain;
+      console.log(socket);
+    }
+  }
+  //next(new Error('Authentication error'));
+});
+*/
+//var nsp = io.of('/prometheus');
 io.on('connection', function(socket){
   console.log('a user connected');
 
   /**
-   * register
+   * This function handles the event 'register' and is used in order to register
+   * a new user. While registering the user the function also adds them to the
+   * public room but in the public room of the client in order to allow
+   * multi-tenancy support
+   *
+   * @method register
+   * @module chat
+   * @submodule prometheus
    */
   socket.on('register',function(data){
-    // namespace
-    /*
-    if (data.namespace !== undefined &&  data.namespace !== '')
-    {
-        socket.namespace = data.namespace;
-    }
-    */
 
     // User information
     var user = {
@@ -32,54 +57,125 @@ io.on('connection', function(socket){
     // Register the user information in the system
     socket.user = user;
 
-    // Add the user to the public room
-    socket.join('public');
+    // Add the user to the public room based on the tenant information
+    if (socket.request.headers.origin) {
+      var matches = url.parse(socket.request.headers.origin).host.match(/(([^.]+)\.)?prometheus\.com/);
+
+      if (matches !== null){
+        socket.account = matches[2];
+
+        // switch the namespace of the socket
+        socket.join('/'+socket.account+'/public');
+        socket.emit('message','Welcome to projects4me');
+
+        io.to('/'+socket.account+'/public').emit('userJoined',user);
+      }
+    }
 
     // Send the user a welcome message
-    socket.emit('message','Welcome to projects4me');
-
-    io.to('public').emit('userJoined',user);
   });
 
+  /**
+   * This function handles the event 'list' and is used in order to retrieve
+   * a list of users in the public room of the tenant that the user belongs to.
+   *
+   * @method list
+   * @module chat
+   * @submodule prometheus
+   */
   socket.on('list',function(){
-    /**
-      Get the list of all the users in the system and return that
-     */
-     console.log('Servig request for list');
-     return [socket.user];
+    var res = [];
+    var ns = io.of("/");
+
+    if (socket.account !== undefined)
+    {
+      var roomId = '/'+socket.account+'/public';
+      if (ns) {
+        for (var id in ns.connected) {
+          if(roomId) {
+            if(ns.connected[id].rooms.public !== undefined)
+            {
+                res.push(ns.connected[id].user);
+                //console.log(ns.connected[id].rooms.public);
+                //console.log(ns);
+            }
+          }
+        }
+      }
+    }
+    res = [socket.user];
+    return res;
   });
 
+  /**
+   * This function handles the event 'joinRoom' and is used in order to join or
+   * create a room for communication. Prometheus is allowed to create rooms for
+   * projects as well as usres.
+   *
+   * @method joinRoom
+   * @module chat
+   * @submodule prometheus
+   */
   socket.on('joinRoom',function(room){
-      socket.join(room);
+    if (socket.account !== undefined)
+    {
+      socket.join('/'+socket.account+'/'+room);
+    }
   });
 
+  /**
+   * This function handles the event 'invite' and is used in order to invite
+   * users to a room
+   *
+   * @method invite
+   * @module chat
+   * @submodule prometheus
+   */
   socket.on('invite',function(data){
+    if (socket.account !== undefined)
+    {
       for (var user in data.users) {
         if (data.hasOwnProperty(id)){
           // Find the user in the socket list
 
-
           // foundSocket.join(data.room);
         }
       }
-  });
-
-  socket.on('createRoom',function(room){
-      if (room !== 'public')
-        socket.join(room);
+    }
   });
 
   socket.on('messageRoom',function(data){
       io.to(data.room).emit('message',{user:socket.user,message:data.message});
   });
 
+  /**
+   * This function handles the event 'message' and is used in order to send
+   * message (to whom?)
+   *
+   * @method message
+   * @module chat
+   * @submodule prometheus
+   */
   socket.on('message',function(message){
     console.log('Sending message:"'+message+'" to room '+socket.room);
       io.to(socket.room).emit('message',{user:socket.user,message:message});
   });
 
+
+  /**
+   * This function handles the event 'disconnect' and is used in order to
+   * disconnect the user from the system and send out a public message
+   * indicating that the user has left.
+   *
+   * @method invite
+   * @module chat
+   * @submodule prometheus
+   */
   socket.on('disconnect',function(){
-    io.to('public').emit('userLeft',{user:socket.user});
+    if (socket.account !== undefined)
+    {
+      io.to('/'+socket.account+'/public').emit('userLeft',{user:socket.user});
+    }
   });
 
 
